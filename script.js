c15303ef71ce24e1ca28e1a8f56e8348b1392fcc
Sylathas <37075162+Sylathas@users.noreply.github.com>
2026-04-28 11:49:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ const storage = getStorage();
 const releasesRef = ref(db, '/releases/');
 onValue(releasesRef, (snapshot) => {
     snapshot.forEach((child) => {
-        console.log(child);
+        console.log(child.val(), child.key);
         var name = child.key;
         var cover = child.val().cover;
         var bandcamp = child.val().bandcamp;
